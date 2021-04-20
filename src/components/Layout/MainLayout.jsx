52b978f5125a4627fa967e2aa7be462d2a9a67e2
akinghill <Austinkinghill@gmail.com>
2021-04-20 13:29:38 -0700

    Update MainLayout.jsx

--- src/components/Layout/MainLayout.jsx
+++ src/components/Layout/MainLayout.jsx
@@ -1,16 +1,18 @@
 import React from "react"
 import HeaderNav from "../HeaderNav"
-import SEO from '../SEO'
+import SEO from "../SEO"
 
 const pageContainer = {
-  "marginTop": "70px",
+  marginTop: "70px",
 }
 
 export default function MainLayout(props) {
+  const url = typeof window !== "undefined" ? window.location.pathname : ""
+
   return (
     <>
       <HeaderNav />
-      <SEO title={props.location.pathname}/>
+      <SEO title={url} />
       <div style={pageContainer}>{props.children}</div>
     </>
   )
